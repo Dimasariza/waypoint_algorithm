f1a1d259866b36e3da9922403885eca74b2c1414
Dimasariza <roganda.sihombing11@gmail.com>
2022-04-29 10:24:38 +0700

    Roganda Dimas - fixing overlap point

--- mutualConnection.js
+++ mutualConnection.js
@@ -1,18 +1,37 @@
-const width = 600;
+const width = 800;
 const height = 600;
-const noParticles = 900;
+
+
+const noParticles = 1200;
 const particleRad = 2;
-const origin = { xOrigin : 20 , yOrigin : 20 }
-const destination = { xDestination : 580 , yDestination : 580 }
+const origin = { xOrigin : 20 , yOrigin : 50 }
+const destination = { xDestination : 650 , yDestination : 580 }
 var circles = new Array;
 console.log(circles)
 // const maxSpeed = 3;
 // const distParticles = 100;
+// var width = [], height = [];
 
 function setup(){
-    createCanvas(width,height)
+
+    // displayWindowSize(w, h)
+    // width.push(w)
+    // height.push(h)
+
+
+
+
+    // const wrapper = document.getElementById('wrapper');
+    // const height = (wrapper.clientHeight)
+    // const width = (wrapper.clientWidth)
+
+
     
-    // Define circle identity the particles
+    var mycanvas = createCanvas(width, height)
+    mycanvas.addClass('canvas1')
+    mycanvas.parent("wrapper")
+
+    background(210)
     while (circles.length < noParticles) {
 
         var circle = {
@@ -24,8 +43,7 @@ function setup(){
         for ( let j = 0; j < circles.length ; j++) {
             var other = circles[j];
             var distance = dist(circle.x, circle.y, other.x, other.y);
-            console.log(distance)
-            if (distance < (2 * particleRad)) {
+            if (distance < (4 * particleRad)) {
                 overlapping = true
             }
         }
@@ -43,10 +61,30 @@ function setup(){
     }
 
     fill('blue')
-    ellipse(origin.xOrigin , origin.yOrigin, 20)
-    ellipse(destination.xDestination, destination.yDestination, 20)
+    ellipse(origin.xOrigin , origin.yOrigin, 10)
+    ellipse(destination.xDestination, destination.yDestination, 10)
 }
 
-// function draw () {
-//     background('rgba(0,0,0,0.1)')
-// }
+function draw () {
+    // background('rgba(0,0,0,0.1)')
+}
+
+
+
+function displayWindowSize(panjang, lebar){
+    // Get width and height of the window excluding scrollbars
+     panjang = document.documentElement.clientWidth;
+     lebar = document.documentElement.clientHeight;
+    
+     console.log(panjang)
+     console.log(lebar)
+    // Display result inside a div element
+    document.getElementById("result").innerHTML = "Width: " + panjang + ", " + "Height: " + lebar;
+}
+    
+// Attaching the event listener function to window's resize event
+// window.addEventListener("resize", displayWindowSize);
+
+// displayWindowSize()
+
+
